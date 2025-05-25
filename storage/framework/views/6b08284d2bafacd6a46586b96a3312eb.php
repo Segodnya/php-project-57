d2bf3b9f6b839e35d879902bf5677315890e8311
Kirill Kurentsov <kkurentsov@team.amocrm.com>
2025-05-25 19:52:56 +0300

    Remove unused Blade view files to clean up the project structure.

--- storage/framework/views/6b08284d2bafacd6a46586b96a3312eb.php
+++
@@ -1,5 +0,0 @@
-<svg viewBox="0 0 317 48" fill="none" xmlns="http://www.w3.org/2000/svg" <?php echo e($attributes); ?>>
-  <path d="M158.5 0L198.125 48H118.875L158.5 0Z" fill="#FF2D20"/>
-  <path d="M79.25 48L118.875 0H39.625L79.25 48Z" fill="#FF2D20"/>
-  <path d="M237.75 48L277.375 0H198.125L237.75 48Z" fill="#FF2D20"/>
-</svg> <?php /**PATH /Users/segodnya/Desktop/hexlet/php-project-57/resources/views/components/application-logo.blade.php ENDPATH**/ ?>
