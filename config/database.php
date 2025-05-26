e3ce220efde75eec94cf0b68be2d3a210f2189ed
Kirill Kurentsov <kkurentsov@team.amocrm.com>
2025-05-26 23:02:39 +0300

    Update database configuration to change SSL mode from 'require' to 'prefer'; delete obsolete view file from storage framework.

--- config/database.php
+++ config/database.php
@@ -75,7 +75,7 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => 'require',
+            'sslmode' => 'prefer',
             'options' => [
                 PDO::ATTR_PERSISTENT => false,
                 PDO::ATTR_TIMEOUT => 60,
